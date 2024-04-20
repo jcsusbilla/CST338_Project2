3a3b47938bd9996d3f5cc7ab4a043dcf5720ba0f
jc <jsusbilla@csumb.edu>
2024-04-20 00:11:17 -0700

    xxx

---
+++ app/src/main/java/com/example/cst338_project2/LandingActivity.java
@@ -0,0 +1,13 @@
+package com.example.cst338_project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
